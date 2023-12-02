556e1a2a3fb46583cfb3130003f6559ff3883d00
yuliiakryvokhyzha <152004676+yuliiakryvokhyzha@users.noreply.github.com>
2023-12-02 19:10:12 +0100

    Update 0001_CreateDB.sql

--- 0001_CreateDB.sql
+++ 0001_CreateDB.sql
@@ -6,24 +6,20 @@ Id INT,
 Nazwa VARCHAR (50) NOT NULL,
 Opis VARCHAR (100),
 Kiedy_obejrzec DATE,
+Obejrzane BIT,
 PRIMARY KEY (Nazwa)
 );
 
-INSERT INTO Harmonogram_filmow (Id, Nazwa, Opis, Kiedy_obejrzec)
 
-VALUES (1, „Klause”, „O tym skąd się wziął Klause, bardzo logiczna historia jak w rzeczywistości mógł istnieć święty Mikołaj”, 2023-12-1);
+CREATE TABLE Aktorzy (
+Id INT, 
+Aktor VARCHAR (50),
+Data_urodzenia INT,
+Kraj_urodzenia VARCHAR (50),
+Oskar BIT,
+PRIMARY KEY (Id));
 
-
-ALTER TABLE Harmonogram_filmow ADD COLUMN Obejrzane BIT; 
-
-
-INSERT INTO Harmonogram_filmow (Id, Nazwa, Opis)
-VALUES (2, "Misia", "Polski film");
-
-CREATE TABLE Aktorzy (Id INT, 
-Nazwa_filmu VARCHAR (50) NOT NULL, 
-Glowne_aktorzy_male VARCHAR (50) NOT NULL, 
-Glowne_aktorzy_female VARCHAR (50) NOT NULL, 
-aktorzy_male VARCHAR (100), 
-aktorzy_female VARCHAR (100),
-PRIMARY KEY (Nazwa_filmu));
+CREATE TABLE Relacje_aktorzy-filmy (
+Nr INT,
+Nazwa_filmu VARCHAR (50),
+Aktor VARCHAR (50));
